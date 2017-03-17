6dd740e92bfa20755108c94e8d0936c54c7b5008
Thibault Clérice <leponteineptique@gmail.com>
2017-03-17 08:59:07 +0100

    Fixed colon

--- modules/capitains/manifests/nginx.pp
+++ modules/capitains/manifests/nginx.pp
@@ -2,14 +2,14 @@
 class capitains::nginx {
   class { 'nginx': 
 
-    gzip                => "on";
-    gzip_disable        => "msie6";
-    gzip_vary           => "on";
-    gzip_proxied        => "any";
-    gzip_comp_level     => "6";
-    gzip_buffers        => "16 8k";
-    gzip_http_version   => "1.1";
-    gzip_types          => ["text/plain", "text/css", "application/json", "application/x-javascript", "text/xml", "application/xml", "application/xml+rss", "text/javascript"];
+    gzip                => "on",
+    gzip_disable        => "msie6",
+    gzip_vary           => "on",
+    gzip_proxied        => "any",
+    gzip_comp_level     => "6",
+    gzip_buffers        => "16 8k",
+    gzip_http_version   => "1.1",
+    gzip_types          => ["text/plain", "text/css", "application/json", "application/x-javascript", "text/xml", "application/xml", "application/xml+rss", "text/javascript"],
   }
 
   nginx::resource::upstream { 'upstream_app':
